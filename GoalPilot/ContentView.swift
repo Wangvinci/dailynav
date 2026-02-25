061d98457fc96375ce77a8834662f69231c879ca
Vince <wangst1994@gmail.com>
2026-02-26 03:00:35 +0800

    Initial Commit

---
+++ GoalPilot/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GoalPilot
+//
+//  Created by Shengtao Wang on 26/02/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GoalPilot/GoalPilotApp.swift
@@ -0,0 +1,17 @@
+//
+//  GoalPilotApp.swift
+//  GoalPilot
+//
+//  Created by Shengtao Wang on 26/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct GoalPilotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
